65e36e4511477b079e31b8bd3bb7a361748ac974
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-09-29 12:55:10 -1000

    added url to axios post to payment intent.

--- frontend/src/api/stripe.js
+++ frontend/src/api/stripe.js
@@ -4,6 +4,6 @@ import apiUrl from '../apiConfig'
 // add an item to the users cart 
 export const createPaymentIntent = (user) => {
   let data = ''
-  return axios.post(apiUrl + `/create-payment-intent`, data,
+  return axios.post(`https://lastwave-ecommerce.herokuapp.com/create-payment-intent`, data,
    { headers: { Authorization: `Bearer ${user.token}`} })
 }
