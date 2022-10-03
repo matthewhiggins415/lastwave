e23fe185d1a68ef3f171deaaa096280e11d817a6
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-10-03 11:29:14 -1000

    changed some stuff.

--- frontend/src/api/stripe.js
+++ frontend/src/api/stripe.js
@@ -4,6 +4,7 @@ import apiUrl from '../apiConfig'
 // add an item to the users cart 
 export const createPaymentIntent = (user) => {
   let data = ''
-  return axios.post(`https://lastwave-ecommerce.herokuapp.com/create-payment-intent`, data,
-   { headers: { Authorization: `Bearer ${user.token}`} })
+  let url = apiUrl + '/create-payment-intent'
+  console.log("url", url)
+  return axios.post(url, data, { headers: { Authorization: `Bearer ${user.token}`} })
 }
