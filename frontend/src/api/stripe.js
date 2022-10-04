69fbd9200f16095d30cdc42aae6d8e259964c2f5
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-10-04 12:22:27 -1000

    fixed get route.

--- frontend/src/api/stripe.js
+++ frontend/src/api/stripe.js
@@ -3,6 +3,5 @@ import apiUrl from '../apiConfig'
 
 // add an item to the users cart 
 export const createPaymentIntent = (user) => {
-  let data = ''
-  return axios.get(apiUrl + '/createintent', data)
+  return axios.get(apiUrl + '/createintent')
 }
