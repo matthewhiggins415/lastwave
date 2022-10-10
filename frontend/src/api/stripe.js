64301f39d429eef6b33dc7ddaa181d57f7ae5960
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-10-10 16:21:37 -0600

    stripped the createintent route.

--- frontend/src/api/stripe.js
+++ frontend/src/api/stripe.js
@@ -3,8 +3,5 @@ import apiUrl from '../apiConfig'
 
 // add an item to the users cart 
 export const createPaymentIntent = (user) => {
-  return axios.get(apiUrl + '/createintent', {
-    withCredentials: true,
-    headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' }
-  })
+  return axios.get(apiUrl + '/createintent')
 }
