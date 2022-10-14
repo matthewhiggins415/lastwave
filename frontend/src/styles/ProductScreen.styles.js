9548cb8ad961fb30a241b09cfef29101a373440e
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-10-14 10:55:16 -0700

    fixed mobile for product details page.

--- frontend/src/styles/ProductScreen.styles.js
+++ frontend/src/styles/ProductScreen.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Link } from 'react-router-dom'
 
 export const Container = styled.div`
-  width: 80%;
+  width: 100%;
   margin: 0 auto;
   margin-top: 30px;
   display: flex;
