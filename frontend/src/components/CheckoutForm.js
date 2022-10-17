04b3069d60052f7aa8d362061ef69a505be15492
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-10-17 14:04:18 -0700

    added https to return url.

--- frontend/src/components/CheckoutForm.js
+++ frontend/src/components/CheckoutForm.js
@@ -62,7 +62,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `www.thesimplecat.com/payment-complete`,
+        return_url: `https://www.thesimplecat.com/payment-complete`,
       },
     });
 
